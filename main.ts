30a759930c285192c5bcf37e0946008bb354f7f9
13enjamin1 <s26343@clphs.edu.my>
2026-06-18 11:41:52 +0800

    help me make a table lamp that ons when i press digital button

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 input.onPinPressed(TouchPin.P0, function () {
-    if (pins.digitalReadPin(DigitalPin.P0) == pins.digitalReadPin(DigitalPin.P0)) {
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
         pins.digitalWritePin(DigitalPin.P8, 1)
-    } else if (false) {
-        pins.digitalWritePin(DigitalPin.P8, 0)
-    } else {
-    	
     }
+    if (!(pins.digitalReadPin(DigitalPin.P0) == 1)) {
+        pins.digitalWritePin(DigitalPin.P8, 0)
+    }
+})
+basic.forever(function () {
+	
 })
